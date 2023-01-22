eb69b865025fbb298ae1fe8175a345c4a96b477b
Saglara <nohaevass@yandex.ru>
2023-01-22 14:58:40 +0300

    Update Ex_two.java

--- Ex_two.java
+++ Ex_two.java
@@ -1,3 +1,9 @@
+/* Задача 2. Вывести все простые числа от 1 до 1000 */
+
 public class Ex_two {
-    
+    public static void main(String[] args) {         
+        for (int i = 1; i <= 1000; i++) {
+            
+        }
+    }
 }
