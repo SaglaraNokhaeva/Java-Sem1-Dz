5356f0a3797d23c3aa648266dc6ba717f4975644
Saglara <nohaevass@yandex.ru>
2023-01-22 17:27:47 +0300

    Update Ex_three.java

--- Ex_three.java
+++ Ex_three.java
@@ -1,20 +1,28 @@
+
 //Задача 3. Реализовать простой калькулятор
 import java.util.Scanner;
 
 public class Ex_three {
     public static void main(String[] args) {
-        System.out.printf("Введите действие (+, -, * или /): ");
-        String action = iScanner.nextLine();
+
         Scanner iScanner = new Scanner(System.in);
+
         System.out.printf("Введите первое число = ");
         int number1 = iScanner.nextInt();
+
         System.out.printf("Введите второе число = ");
         int number2 = iScanner.nextInt();
+
        
-        //int rezult = 0;
-        if (action == "+") {
-            System.out.printf("%d + %f = %g", number1, number2, number1 + number2);
+        System.out.printf("Введите действие (+, -, * или /): ");
+        String act = iScanner.next();
+
+        if (act == "+") {
+            // int sum=number1+number2;
+            // System.out.print(act);
+            System.out.print(number2);
         }
+        System.out.print(act);
         iScanner.close();
     }
 }
